0c8fe0c8958b4d467dcd734840f23ab2b6242f9c
James <james.kariuki5@student.moringaschool.com>
2025-08-12 15:10:26 +0300

    initial commit

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+# lib/anagram.py
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()  # lowercase to avoid case mismatch
+
+    def match(self, words):
+        matches = []
+        for candidate in words:
+            if candidate.lower() != self.word:  # avoid identical match
+                if sorted(candidate.lower()) == sorted(self.word):
+                    matches.append(candidate)
+        return matches
